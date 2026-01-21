fa6d37103ed3a58b556fedb51a8bfef2a0c90b90
Cristian Razo <crandres240992@gmail.com>
2026-01-21 04:04:21 -0600

    Correccion de defectos

--- src/test/java/com/crisscript/GreetingResourceTest.java
+++ src/test/java/com/crisscript/GreetingResourceTest.java
@@ -4,17 +4,17 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-class GreetingResourceTest {
-    @Test
-    void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
-    }
+public class GreetingResourceTest {
 
+    @Test
+    public void testDealsEndpointExists() {
+        // Verificamos que el endpoint responda, aunque sea un 415 (Unsupported Media Type)
+        // porque no le estamos enviando un JSON, pero esto confirma que la app inició y el path existe.
+        given()
+          .when().post("/v1/deals/filter")
+          .then()
+             .statusCode(415); 
+    }
 }
